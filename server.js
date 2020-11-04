dbb84753b2d7199b09c6ebf96aa3ae440b737a83
Tyna Nguyen <ngtyna@gmail.com>
2020-11-04 15:15:17 -0600

    Retrieve a single employee from the database by id

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ app.get("/api/employees", (req, res) => {
   // Log to the node server
   // console.log("Successful GET route for all the employees");
 
-  // Respond will all employees from the database
+  // Respond with all employees from the database
   queries.retrieveAllEmployees((err, results) => {
     // console.log("Results from GET request: ", results);
     if (err) {
@@ -29,7 +29,16 @@ app.get("/api/employees", (req, res) => {
 
 app.get("/api/employees/:id", (req, res) => {
   // console.log("Successful GET route for a single employee")
-  res.send("single employee");
+  // console.log("Value of id property on req.params:", req.params.id);
+
+  let employeeId = req.params.id;
+  // Respond with a single employee from the database by id
+  queries.retrieveEmployee(employeeId, (err, results) => {
+    if (err) {
+      res.status(400).send(err);
+    }
+    res.status(200).send(results);
+  })
 })
 
 // Listen on port for connections
