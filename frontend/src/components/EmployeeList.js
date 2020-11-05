2bb058eccfab6a0ddc69c5126d31d699dab3ba97
Tyna Nguyen <ngtyna@gmail.com>
2020-11-04 20:03:46 -0600

    Iterate through the dummy data to display a list of employee components

--- frontend/src/components/EmployeeList.js
+++ frontend/src/components/EmployeeList.js
@@ -1,12 +1,13 @@
 import React from "react";
 import Employee from "./Employee";
 
-const EmployeeList = () => {
+const EmployeeList = (props) => {
+  // console.log("Dummy data props passed from App:", props);
   return (
     <div>
-      <Employee />
-      <Employee />
-      <Employee />
+      {props.employees.map((employee) => {
+        return <Employee employee={employee} key={employee.id}/>
+      })}
     </div>
   );
 };
