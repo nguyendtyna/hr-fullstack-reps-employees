77df8e3af03368446ac0016a69baf98e81eb13fe
Tyna Nguyen <ngtyna@gmail.com>
2020-11-04 20:06:53 -0600

    Dynamically render each individual employee's data

--- frontend/src/components/Employee.js
+++ frontend/src/components/Employee.js
@@ -1,11 +1,17 @@
 import React from "react";
 
-const Employee = () => (
-  <div>
-    {/* Single Employee here  */}
-    <h3>Nik Mentakis</h3>
-    <p>Austin</p>
-  </div>
-);
+const Employee = (props) => {
+  // Access props to get exampleEmployee data: props ==> employee
+  // console.log("Employee obj from employeeList:", props);
+  // console.log("Employee's first name:", props.employee.first_name);
+
+  return (
+    <div>
+      {/* Single Employee here  */}
+      <h3>{props.employee.first_name} {props.employee.last_name}</h3>
+      <p>{props.employee.city}</p>
+    </div>
+  )
+};
 
 export default Employee;
